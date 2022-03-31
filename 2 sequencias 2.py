7768e88d2dda6535ea92830a54bc4382eeb82a93
Keanu Sales <100793959+keanusales@users.noreply.github.com>
2022-03-31 11:27:59 -0300

    Update 2 sequencias 2.py

--- 2 sequencias 2.py
+++ 2 sequencias 2.py
@@ -1,33 +1,32 @@
-import random
-import matplotlib.pyplot as m
+import random as rdm
+import matplotlib.pyplot as plt
 v1, v2, v3, f1 = [], [], [], 0
-s1, s2, vt, f2 = [], [], [], 0
+vt, s1, s2, f2 = [], [], [], 0
 for i in range(100):
-    v3.insert(i, i)
+    v1.insert(i, i)
 for i in range(100):
     vt.insert(i, i)
 for i in range(100):
-    random.shuffle(vt)
+    rdm.shuffle(vt)
     for j in range(100):
-        v1.insert(j, 0)
         v2.insert(j, 0)
-    v1.insert(vt[i], 1)
+        v3.insert(j, 0)
+    v2.insert(vt[i], 1)
     for j in range(100):
-        if v1[j] != v2[j]:
+        if v2[j] != v3[j]:
             s1.insert(i, j + 1)
             break
-    random.shuffle(vt)
+    rdm.shuffle(vt)
     for j in range(100):
-        if v1[vt[j]] != v2[vt[j]]:
+        if v2[vt[j]] != v3[vt[j]]:
             s2.insert(i, j + 1)
             break
-m.plot(v3, s1, color="Red")
-m.plot(v3, s2, color="Blue")
-m.show()
+plt.plot(v1, s1, color="Red")
+plt.plot(v1, s2, color="Blue")
+plt.show()
 for i in range(100):
     f1 = f1 + s1[i]
     f2 = f2 + s2[i]
-f1 = f1 / 100
-f2 = f2 / 100
+f1, f2 = f1 / 100, f2 / 100
 print("Sequencialmente a média foi:", f1)
 print("Aleatoriamente a média foi:", f2)
