63a78d39eb55de38b86095fbab294a4c752c8372
Keanu Sales <100793959+keanusales@users.noreply.github.com>
2022-04-02 18:19:49 -0300

    Update 2 sequencias 2.py

--- 2 sequencias 2.py
+++ 2 sequencias 2.py
@@ -3,26 +3,26 @@ import matplotlib.pyplot as plt
 v1, v2, v3, f1 = [], [], [], 0
 vt, s1, s2, f2 = [], [], [], 0
 for i in range(100):
-    v1.insert(i, i)
-for i in range(100):
     vt.insert(i, i)
 for i in range(100):
     rdm.shuffle(vt)
     for j in range(100):
+        v1.insert(j, 0)
         v2.insert(j, 0)
-        v3.insert(j, 0)
-    v2.insert(vt[i], 1)
+    v1.insert(vt[i], 1)
     for j in range(100):
-        if v2[j] != v3[j]:
+        if v1[j] != v2[j]:
             s1.insert(i, j + 1)
             break
     rdm.shuffle(vt)
     for j in range(100):
-        if v2[vt[j]] != v3[vt[j]]:
+        if v1[vt[j]] != v2[vt[j]]:
             s2.insert(i, j + 1)
             break
-plt.plot(v1, s1, color="Red")
-plt.plot(v1, s2, color="Blue")
+for i in range(100):
+    v3.insert(i, i + 1)
+plt.plot(v3, s1, color = "r")
+plt.plot(v3, s2, color = "k")
 plt.show()
 for i in range(100):
     f1 = f1 + s1[i]
