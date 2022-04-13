5f8dbb5ce3501b1e5f1e76b4be0777d48a8f4462
Keanu Sales <100793959+keanusales@users.noreply.github.com>
2022-04-13 11:15:15 -0300

    v4

--- Lista 1/Códigos Lista 1/2 sequencias 1.cpp
+++ Lista 1/Códigos Lista 1/2 sequencias 1.cpp
@@ -2,39 +2,40 @@
 using namespace std;
 int main()
 {
-    int t1, t2, i, v = 0, q = 0;
+    int t1, t2, i, v = 0;
     cout << "Qual a quantidade de termos da primeira sequencia? ";
-    cin >> t1;
+    cin >> t1; //Entra com o tamanho da 1ª sequência
     cout << "Qual a quantidade de termos da segunda sequencia? ";
-    cin >> t2;
-    int v1[t1], v2[t2];
+    cin >> t2; //Entra com o tamanho da 2ª sequência
     if (t1 == t2)
-    {
+    { //Se os tamanhos são iguais, os vetores podem ser iguais
+        int v1[t1], v2[t2]; //Faz os vetores com os tamanhos
         for(i = 0; i < t1; i++)
-        {
-            cout << "Digite o termo " << i+1 << " da sequencia 1: ";
+        { //Recebe os termos, um por um, do vetor 1
+            cout << "Digite o " << i+1 << "º termo da sequencia 1: ";
             cin >> v1[i];
         }
         for(i = 0; i < t2; i++)
-        {
-            cout << "Digite o termo " << i+1 << " da sequencia 2: ";
+        { //Recebe os termos, um por um, do vetor 2
+            cout << "Digite o " << i+1 << "º termo da sequencia 2: ";
             cin >> v2[i];
         }
         for(i = 0; i < t1; i++)
-        {
+        { //Verifica, índice a índice, se os vetores são iguais
             if (v1[i] != v2[i])
-            {
-                v = 1;
-                break;
-            }
+            { v = 1; break; }
+            /*Caso sejam diferentes, v é alterdo pra 1, já
+            que inicialmente os vetores são iguais, até se
+            provar o contrário*/
         }
-        q = q + i + 1;
-        if (v == 0) cout << "As sequencias sao iguais.\n";
-        else cout << "As sequencias nao sao iguais.\n";
-        cout << "A quantidade de casas percorridas foi " << q << ".";
+        if (v == 0) cout << "As sequencias são iguais.\n";
+        //Se v é igual a 0, então os vetores são iguais
+        else cout << "As sequencias não são iguais.\n";
+        //Caso contrário, os vetores são diferentes
+        cout << "A quantidade de loops necessários foi " << i << ".";
+        /*Com o último índice i pode-se saber a quantidade
+        de loops que foram necessários para a verificação*/
     }
-    else
-    {
-        cout << "As sequencias nao sao iguais.";
-    }
+    else cout << "As sequencias não são iguais.";
+    //Caso contrário, os vetores são diferentes
 }
--- Lista 1/Códigos Lista 1/2 sequencias 2.py
+++ Lista 1/Códigos Lista 1/2 sequencias 2.py
@@ -1,26 +1,41 @@
 import random as rdm
+# Importa a biblioteca para randomizar coisas
 import matplotlib.pyplot as plt
+# Importa a biblioteca para criar e mostar gráficos
 v1, v2, v3, f1 = [], [], [], 0
 vt, s1, s2, f2 = [], [], [], 0
 for i in range(100):
-    vt.insert(i, i)
+    vt.append(i)
+# Cria um vetor vt com os índices 0 a 100 dos
+# vetores, que futuramente vai ser embaralhado
 for i in range(100):
-    rdm.shuffle(vt)
+# Faz 100 vezes o processo de criar 2 vetores
     for j in range(100):
         v1.insert(j, 0)
         v2.insert(j, 0)
+    # Cria os 2 vetores com 100 índices cada
+    rdm.shuffle(vt)
+    # O vetor vt é embaralhado randomicamente
     v1.insert(vt[i], 1)
+    # Insere "1" no vetor v1 com um índice
+    # aleatório definido por vt[i], onde nesse
+    # vt[i] há um número aleatório entre 0 e 100,
+    # já que vt foi embaralhado randomicamente
     for j in range(100):
         if v1[j] != v2[j]:
             s1.insert(i, j + 1)
             break
+    # Percorre sequencialmente os 2 vetores até
+    # achar o ponto em que são diferentes e
+    # armazena esse ponto em um outro vetor
     rdm.shuffle(vt)
+    # O vetor vt é embaralhado randomicamente
     for j in range(100):
         if v1[vt[j]] != v2[vt[j]]:
             s2.insert(i, j + 1)
             break
 for i in range(100):
-    v3.insert(i, i + 1)
+    v3.append(i + 1)
 plt.plot(v3, s1, color = "r")
 plt.plot(v3, s2, color = "k")
 plt.show()
